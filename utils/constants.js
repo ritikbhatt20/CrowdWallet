a91da659797fa16e4d04f43cfc70c69f245a32a5
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-03-26 03:46:36 +0530

    🚀Added constants, helper and idl files

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const PROJECT_SEED = "project";
+
+export const PROGRAM_ID = new PublicKey(
+    "3CrwMLJ3TbYdi7Rc2sBjsnB6reGQUAvf4TRKTwkP5uc3"    
+);  
